53e3155148c68a9a5b972d6886d31828f18f1fbf
Ramin Taghizada <ramintagizade@mail.ru>
2017-05-25 15:53:00 +0200

    React Routers

--- 2-react-router/src/js/pages/Layout.js
+++ 2-react-router/src/js/pages/Layout.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Link} from "react-router";
-//import Footer from "../components/layout/Footer";
+import Footer from "../components/layout/Footer";
 import Nav from "../components/layout/Nav";
 export default class Layout extends React.Component {
 
@@ -20,7 +20,7 @@ export default class Layout extends React.Component {
               {this.props.children}
             </div>
           </div>
-
+            <Footer/>
         </div>
       </div>
     );
